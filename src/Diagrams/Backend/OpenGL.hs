24c3b5e8c088a8100300c5b7675f8f1befa4979c
Daniel Bergey <bergey@alum.mit.edu>
2013-03-19 22:29:47 -0400

    Fit modelView matrix to Diagram size
    include bounding box of Diagram in Render OpenGL R2
    Set matrix as part of doRender
    add semigroups to dependencies
    
    The 5% padding in inclusiveOrtho is arbitrary, should be configurable

--- src/Diagrams/Backend/OpenGL.hs
+++ src/Diagrams/Backend/OpenGL.hs
@@ -7,7 +7,7 @@
 module Diagrams.Backend.OpenGL where
 
 -- General Haskell
-import Prelude
+import Data.Semigroup
 import           Data.Typeable
 import qualified Data.Vector.Storable as V
 
@@ -20,7 +20,8 @@ import Diagrams.Prelude hiding (Attribute, close, e, (<>))
 import Graphics.Rendering.Util
 
 renderPath :: Path R2 -> Render OpenGL R2
-renderPath (Path trs) = GlRen $ fmap renderTrail trs
+renderPath (Path trs) = GlRen box $ fmap renderTrail trs where
+  box = mconcat . map (boundingBox . snd) $ trs
 
 renderTrail :: (P2, Trail R2) -> GlPrim
 renderTrail (p0, t) = GlPrim mode white vertices where
@@ -32,14 +33,17 @@ renderTrail (p0, t) = GlPrim mode white vertices where
 flatP2 :: (Fractional a, Num a) => P2 -> [a]
 flatP2 (unp2 -> (x,y)) = [r2f x, r2f y]
 
-r2f :: (Real a, Fractional b) => a -> b
+r2f :: (Real r, Fractional f) => r -> f
 r2f x = realToFrac x
+
+r2fPr :: (Real r, Fractional f) => (r,r) -> (f,f)
+r2fPr (a,b) = (r2f a, r2f b)
 
 data OpenGL = OpenGL
             deriving (Show, Typeable)
 
 instance Backend OpenGL R2 where
-  data Render OpenGL R2 = GlRen [GlPrim]
+  data Render OpenGL R2 = GlRen (BoundingBox R2) [GlPrim]
                         deriving Show
   type Result OpenGL R2 = IO ()
   data Options OpenGL R2 = GlOptions
@@ -55,15 +59,18 @@ instance Backend OpenGL R2 where
   doRender _ _ = displayPrims
 
 displayPrims :: Render OpenGL R2 -> IO ()
-displayPrims (GlRen ps) =do
+displayPrims (GlRen b ps) = do
     clearColor $= glColor (black :: Colour GLfloat)
     clear [ColorBuffer]
+    matrixMode $= Modelview 0
+    loadIdentity
+    inclusiveOrtho b
     mapM_ (drawOGL 2) ps
     flush
 
 instance Monoid (Render OpenGL R2) where
-  mempty = GlRen mempty
-  (GlRen r0) `mappend` (GlRen r1) = GlRen $ r0 `mappend` r1
+  mempty = GlRen emptyBox mempty
+  (GlRen b0 r0) `mappend` (GlRen b1 r1) = GlRen (b0 <> b1) (r0 <> r1)
 
 instance Renderable (Path R2) OpenGL where
   render _ = renderPath
@@ -79,3 +86,13 @@ dimensions = unr2 . boxExtents . boundingBox
 
 aspectRatio :: QDiagram b R2 m -> Double
 aspectRatio = uncurry (/) . dimensions
+
+inclusiveOrtho :: BoundingBox R2 -> IO ()
+inclusiveOrtho b = ortho x0 x1 y0 y1 z0 z1 where
+  defaultBounds = (p2 (-1,-1), p2 (1,1))
+  ext      = unr2 $ boxExtents b
+  (ll, ur) = maybe defaultBounds id $ getCorners b
+  (x0, y0) = r2fPr $ unp2 ll - 0.05 * ext
+  (x1, y1) = r2fPr $ unp2 ur + 0.05 * ext
+  z0 = 0
+  z1 = 1
