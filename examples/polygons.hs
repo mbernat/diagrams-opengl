dc65498f996db36fd1f22ca2912c6ebdacf07e79
Daniel Bergey <bergey@alum.mit.edu>
2013-03-21 15:16:15 -0400

    Example with filled polygons

---
+++ examples/polygons.hs
@@ -0,0 +1,24 @@
+import Diagrams.Prelude hiding (doRender)
+import Diagrams.Backend.OpenGL
+import Diagrams.Backend.OpenGL.CmdLine
+
+main :: IO ()
+main = do
+  putStrLn $ show $ renderPath p
+  defaultMain d
+
+v1 :: R2
+v1 = r2 (0,1)
+
+v2 :: R2
+v2 = r2 (0.5,-0.5)
+
+p :: Path R2
+p = close $ fromOffsets [v1,v2]
+
+p2_ :: Path R2
+p2_ = close $ fromOffsets [v2, v1]
+
+d :: Diagram OpenGL R2
+d = stroke p  # fc green <>
+    (stroke p2_ # lc blue # fc cyan # translate (r2 (0,0.5)))
