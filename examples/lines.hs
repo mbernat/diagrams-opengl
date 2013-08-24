2ea7455b532db96f1b6b535cc95372fc1320206a
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2013-08-24 18:10:15 +0200

    Updated the examples for diagrams-0.7 API.

--- examples/lines.hs
+++ examples/lines.hs
@@ -15,11 +15,13 @@ v2 = r2 (0.5,-0.5)
 p :: Path R2
 p = fromOffsets [v1,v2, v1, v2,v1,-v2,v1]
 
-p2_ :: Path R2
-p2_ = fromOffsets [v2, v1]
+p2_, p2_closed :: Path R2
+[p2_, p2_closed] = map pathFromTrail [open, closed]
+ where open = fromOffsets [v2, v1]
+       closed = closeTrail open
 
 d :: Diagram OpenGL R2
 d = stroke p # lc red <>
     stroke p2_ # lc blue <>
     (stroke $ fromOffsets [v2, v2, v1]) <>
-    stroke (close p2_) # lc green # translate (v1+v2)
+    stroke (p2_closed) # lc green # translate (v1+v2)
