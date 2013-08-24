1fcc715b5e787803adc36970bfee902aa03913a9
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2013-08-24 18:10:15 +0200

    Updated the examples for diagrams-0.7 API.

--- examples/polygons.hs
+++ examples/polygons.hs
@@ -4,7 +4,6 @@ import Diagrams.Backend.OpenGL.CmdLine
 
 main :: IO ()
 main = do
-  putStrLn $ show $ renderPath p
   defaultMain d
 
 v1 :: R2
@@ -14,10 +13,10 @@ v2 :: R2
 v2 = r2 (0.5,-0.5)
 
 p :: Path R2
-p = close $ fromOffsets [v1,v2]
+p = pathFromTrail . closeTrail $ fromOffsets [v1,v2]
 
 p2_ :: Path R2
-p2_ = close $ fromOffsets [v2, v1]
+p2_ = pathFromTrail . closeTrail $ fromOffsets [v2, v1]
 
 d :: Diagram OpenGL R2
 d = stroke p  # fc green <>
