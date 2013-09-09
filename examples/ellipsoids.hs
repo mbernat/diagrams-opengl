8779621b41d581882f0442e85dffe2806ca4b36e
Daniel Bergey <bergey@alum.mit.edu>
2013-09-08 20:34:36 -0400

    elaborate ellipsoids example

--- examples/ellipsoids.hs
+++ examples/ellipsoids.hs
@@ -5,12 +5,15 @@ import Diagrams.ThreeD.Types
 
 import Diagrams.ThreeD.Shapes
 
-example :: Diagram OpenGL R3
-example = sphere # translate (r3 (0.8, 0.8, -5)) # scale 0.5 # fc blue
-          <> sphere # translate (r3 (-0.8,-0.8, -2)) # scale 0.5
+collection :: Diagram OpenGL R3
+collection = mconcat $ map around [2*pi/8.0, 4*pi/8.0 .. 2*pi]
+
+around :: Double -> Diagram OpenGL R3
+around θ = translate r . scale 0.1 $ sphere where
+  r = r3 (0.8 * sin θ, 0.8 * cos θ, 0)
 
 main :: IO ()
 main = do
   --print . toDoc $ polarSphere 5 5
   --fileOutputDemo "ellipsoids.png" $ example
-  defaultMain example
+  defaultMain collection
