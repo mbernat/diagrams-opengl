09257e4821a9d42628c6ff5dea5b7b04d52f59d6
Daniel Bergey <bergey@alum.mit.edu>
2014-02-16 21:06:03 +0000

    examples: update lineForm for v1.0

--- examples/lineForm.hs
+++ examples/lineForm.hs
@@ -6,7 +6,7 @@ path :: Diagram OpenGL R2
 path = fromVertices (map p2 [(0,0), (1,0.3), (2,0), (2.2,0.3)]) # lw 0.1
 
 example :: Diagram OpenGL R2
-example = centerXY . vcat' with { sep = 0.1 }
+example = centerXY . vcat' (with & sep .~ 0.1 )
           $ map (path #)
             [ lineCap LineCapButt   . lineJoin LineJoinMiter
             , lineCap LineCapRound  . lineJoin LineJoinRound
