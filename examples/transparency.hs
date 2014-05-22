940a14615d56cb6ce6de7d93d22ff26c0a145944
Daniel Bergey <bergey@alum.mit.edu>
2014-05-22 02:57:16 +0000

    Use lenses to access Diagrams records

--- examples/transparency.hs
+++ examples/transparency.hs
@@ -3,7 +3,7 @@ import Diagrams.Backend.OpenGL
 import Diagrams.Backend.OpenGL.CmdLine
 
 colors  = map (blue `withOpacity`) [0.1, 0.2 .. 1.0]
-example = hcat' with { catMethod = Distrib, sep = 1 }
+example = hcat' (with & catMethod .~ Distrib & sep .~ 1 )
           (zipWith fcA colors (repeat (circle 1)))
 
 main = defaultMain example
